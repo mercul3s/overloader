bf1a3b0c502e440d38eb574e31eaafe7cb6351a2
Mercedes Coyle <mcoyle@ulive.com>
2015-02-18 13:07:52 -0800

    Finished adding tests for object initialization

--- spec/sender_spec.rb
+++ spec/sender_spec.rb
@@ -2,14 +2,21 @@ require 'rspec'
 load 'lib/sender.rb'
 
 describe 'Sender', "#initialize" do
-  
+  sender = Sender.new('http://example.com')
+
   it "has a request_time variable initialized to nil" do
-    sender = Sender.new('http://example.com')
     expect(sender.request_time).to eq nil
   end
 
   it "has a response variable initialized to nil" do
-    sender = Sender.new('http://example.com')
     expect(sender.response).to eq nil
+  end
+
+  it "has a timer variable initialized with a timer object" do
+    expect(sender.instance_variable_get(:@timer)).to be_an_instance_of(Timer)
+  end
+
+  it "has a resource variable initialized with a RestClient object" do
+    expect(sender.instance_variable_get(:@resource)).to be_an_instance_of(RestClient::Resource)
   end
 end
